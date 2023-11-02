d001abeb28d74b7d4176413aa1b5be4410915eb0
Technoblade <william.giles2005@gmail.com>
2023-11-02 23:45:05 +1300

    Reverse order of projects in Projects component

--- components/home/projects/Projects.tsx
+++ components/home/projects/Projects.tsx
@@ -1,19 +1,19 @@
-import { SectionHeader } from "@/components/utils/SectionHeader";
-import { Project } from "./Project";
+import { SectionHeader } from '@/components/utils/SectionHeader'
+import { Project } from './Project'
 
-import { projects } from "@/lib/projects";
+import { projects } from '@/lib/projects'
 
 export const Projects = () => {
+  const reversedProjects = [...projects].reverse()
   return (
     <section className="section-wrapper" id="projects">
       <SectionHeader title="Projects" dir="r" />
 
-      <div className={"grid grid-cols-1 lg:grid-cols-2 gap-8"}>
-        {projects.map((project) => {
-          return <Project key={project.title} {...project} />;
+      <div className={'grid grid-cols-1 lg:grid-cols-2 gap-8'}>
+        {reversedProjects.map((project) => {
+          return <Project key={project.title} {...project} />
         })}
       </div>
     </section>
-  );
-};
-
+  )
+}
