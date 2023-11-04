e0eb18bab5e4cedd42afa8179ff2833e9d7f8540
Technoblade <william.giles2005@gmail.com>
2023-11-05 12:52:58 +1300

    fixed build step

--- next.config.js
+++ next.config.js
@@ -4,16 +4,8 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // experimental: {
-  //   fontLoaders: [
-  //     { loader: '@next/font/google', options: { subsets: ['latin'] } },
-  //   ],
-  // },
 }
 
 // export default withContentlayer(nextConfig)
 module.exports = withContentlayer(nextConfig)
 
-// export default nextConfig
-
-// module.exports = nextConfig;
