7f4cc097b4cf9a01794e6ecf3405692b69584eb9
Technoblade <william.giles2005@gmail.com>
2023-11-05 04:04:57 +1300

    working on mdx
    mostly working
    code highlighting not working fully

--- components/home/projects/Projects.tsx
+++ components/home/projects/Projects.tsx
@@ -1,18 +1,42 @@
 import { SectionHeader } from '@/components/utils/SectionHeader'
 import { Project } from './Project'
 
-import { projects } from '@/lib/projects'
+import { allProjectMDXes } from 'contentlayer/generated'
+import { ProjectMDX } from 'contentlayer/generated'
+
+const parseDate = (dateString: string): Date => {
+  const parts = dateString.split('/') as [string, string, string]
+
+  const day = parseInt(parts[0], 10)
+  const month = parseInt(parts[1], 10) - 1
+  const year = parseInt(parts[2], 10)
+
+  const date = new Date(year, month, day)
+
+  return date
+}
 
 export const Projects = () => {
-  const reversedProjects = [...projects].reverse()
   return (
     <section className="section-wrapper" id="projects">
       <SectionHeader title="Projects" dir="r" />
 
       <div className={'grid grid-cols-1 lg:grid-cols-2 gap-8'}>
-        {reversedProjects.map((project) => {
-          return <Project key={project.title} {...project} />
-        })}
+        {allProjectMDXes
+          .sort((a: ProjectMDX, b: ProjectMDX) => {
+            if (
+              parseDate(a.publishedAt).getTime() >
+              parseDate(b.publishedAt).getTime()
+            ) {
+              return -1
+            }
+            return 1
+          })
+          .map((project: ProjectMDX) => {
+            // console.log(project)
+
+            return <Project key={project.title} {...project} />
+          })}
       </div>
     </section>
   )
---
+++ contentlayer.config.ts
@@ -0,0 +1,118 @@
+import {
+  type ComputedFields,
+  defineDocumentType,
+  makeSource,
+} from 'contentlayer/source-files'
+import remarkGfm from 'remark-gfm'
+import rehypePrettyCode from 'remark-gfm'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+
+const computedFields: ComputedFields<'ProjectMDX'> = {
+  techArr: {
+    type: 'list',
+    resolve: (doc) =>
+      (doc.tech as string)
+        .replaceAll('\\\\', '[BACKSLASH]')
+        .split('\\')
+        .map((s) => s.trim().replaceAll('[BACKSLASH]', '\\')),
+  },
+}
+
+const ProjectMDX = defineDocumentType(() => ({
+  name: 'ProjectMDX',
+  filePathPattern: `**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+
+    title: {
+      type: 'string',
+      required: true,
+    },
+    publishedAt: {
+      type: 'string',
+      required: true,
+    },
+    updatedAt: {
+      type: 'string',
+      required: false,
+    },
+    coverImage: {
+      type: 'string',
+      required: true,
+    },
+    projectLink: {
+      type: 'string',
+      required: false,
+    },
+    codeLink: {
+      type: 'string',
+      required: false,
+    },
+    description: {
+      type: 'string',
+      required: true,
+    },
+    tech: {
+      type: 'string',
+      required: true,
+    },
+  },
+  computedFields,
+}))
+
+interface iNode {
+  children: iNode[]
+  properties: {
+    className: string[]
+  }
+  type: string
+  value: string
+}
+
+export default makeSource({
+  contentDirPath: 'content',
+  documentTypes: [ProjectMDX],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: 'github-dark',
+          keepBackground: false,
+
+          onVisitLine(node: iNode) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty
+            // lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [
+                {
+                  type: 'text',
+                  value: ' ',
+                  properties: { className: [] },
+                  children: [],
+                },
+              ]
+            }
+          },
+          onVisitHighlightedLine(node: iNode) {
+            node.properties.className.push("line--highlighted");
+          },
+          onVisitHighlightedWord(node: iNode) {
+            node.properties.className = ["word--highlighted"];
+          },
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ['anchor'],
+          },
+        },
+      ],
+    ],
+  },
+})
