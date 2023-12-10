7ca162cc0632e961e83d459ed4b72a8a95f6ca78
Technoblade <william@williamgiles.co.nz>
2023-12-10 20:59:15 +1300

    SpeedInsights

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Home } from "@/components/home/Home";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function home() {
   return (
@@ -10,8 +11,8 @@ export default function home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <html className="bg-background"></html> */}
       <Home />
+      <SpeedInsights />
     </>
   );
 }
