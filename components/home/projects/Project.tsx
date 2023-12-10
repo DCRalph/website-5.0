0e609190d9fe710c563efaf2ae71f0eddc3b8d8b
Technoblade <william@williamgiles.co.nz>
2023-12-10 23:38:53 +1300

    clean up

--- components/home/projects/Project.tsx
+++ components/home/projects/Project.tsx
@@ -1,22 +1,22 @@
-import { Reveal } from '@/components/utils/Reveal'
-import { useAnimation, useInView, motion } from 'framer-motion'
-import Link from 'next/link'
-import Image from 'next/image'
-import { useEffect, useRef, useState } from 'react'
-import { AiFillGithub, AiOutlineExport } from 'react-icons/ai'
-import { ProjectModal } from './ProjectModal'
+import { Reveal } from "@/components/utils/Reveal";
+import { useAnimation, useInView, motion } from "framer-motion";
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import { AiFillGithub, AiOutlineExport } from "react-icons/ai";
+import { ProjectModal } from "./ProjectModal";
 
-import { ProjectMDX } from 'contentlayer/generated'
+import { ProjectMDX } from "contentlayer/generated";
 
 export const Project = (project: ProjectMDX) => {
-  const [hovered, setHovered] = useState(false)
+  const [hovered, setHovered] = useState(false);
 
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-  const controls = useAnimation()
+  const controls = useAnimation();
 
-  const ref = useRef(null)
-  const isInView = useInView(ref, { once: true })
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
 
   // let modalContent = project.body
   // let projectLink = project.projectLink
@@ -28,11 +28,11 @@ export const Project = (project: ProjectMDX) => {
 
   useEffect(() => {
     if (isInView) {
-      controls.start('visible')
+      controls.start("visible");
     } else {
-      controls.start('hidden')
+      controls.start("hidden");
     }
-  }, [isInView, controls])
+  }, [isInView, controls]);
 
   return (
     <>
@@ -44,40 +44,43 @@ export const Project = (project: ProjectMDX) => {
         }}
         initial="hidden"
         animate={controls}
-        transition={{ duration: 0.75 }}>
+        transition={{ duration: 0.75 }}
+      >
         <div
           onMouseEnter={() => setHovered(true)}
           onMouseLeave={() => setHovered(false)}
           onClick={() => setIsOpen(true)}
           className={
-            'w-full aspect-video bg-background-light cursor-pointer relative rounded-lg overflow-hidden'
-          }>
+            "relative aspect-video w-full cursor-pointer overflow-hidden rounded-lg bg-background-light"
+          }
+        >
           <Image
             src={project.coverImage}
             alt={`An image of the ${project.title} project.`}
             width={10000}
             height={10000}
-            className="w-10/12 absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-5 transition-all rounded-t-lg"
+            className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-5 rounded-t-lg transition-all"
             style={{
-              width: hovered ? '95%' : '90%',
-              rotate: hovered ? '2deg' : '0deg',
+              width: hovered ? "95%" : "90%",
+              rotate: hovered ? "2deg" : "0deg",
             }}
           />
         </div>
-        <div className={'my-4'}>
+        <div className={"my-4"}>
           <Reveal width="100%">
-            <div className={'flex items-center gap-4'}>
-              <h4 className="font-bold text-lg xl:text-2xl shrink-0 max-w-[calc(100%-120px)]">
+            <div className={"flex items-center gap-4"}>
+              <h4 className="max-w-[calc(100%-120px)] shrink-0 text-lg font-bold xl:text-2xl">
                 {project.title}
               </h4>
-              <div className={'w-full h-1 rounded-full bg-text opacity-30'} />
+              <div className={"h-1 w-full rounded-full bg-text opacity-30"} />
 
               {project.codeLink != undefined && (
                 <Link
                   href={project.codeLink}
                   target="_blank"
                   rel="nofollow"
-                  className="transition-all hover:text-brand">
+                  className="transition-all hover:text-brand"
+                >
                   <AiFillGithub size="2rem" />
                 </Link>
               )}
@@ -87,7 +90,8 @@ export const Project = (project: ProjectMDX) => {
                   href={project.projectLink}
                   target="_blank"
                   rel="nofollow"
-                  className="transition-all hover:text-brand">
+                  className="transition-all hover:text-brand"
+                >
                   <AiOutlineExport size="2rem" />
                 </Link>
               )}
@@ -95,17 +99,19 @@ export const Project = (project: ProjectMDX) => {
           </Reveal>
           <Reveal>
             <div
-              className={'flex flex-wrap gap-2 text-brand my-2 font-semibold'}>
-              {project.techArr.join(' - ')}
+              className={"my-2 flex flex-wrap gap-2 font-semibold text-brand"}
+            >
+              {project.techArr.join(" - ")}
             </div>
           </Reveal>
           <Reveal>
-            <p className={'text-lg'}>
-              {project.description}{' '}
+            <p className={"text-lg"}>
+              {project.description}{" "}
               <span
                 onClick={() => setIsOpen(true)}
-                className=" cursor-pointer font-semibold text-brand hover:underline">
-                Learn more {'>'}
+                className=" cursor-pointer font-semibold text-brand hover:underline"
+              >
+                Learn more {">"}
               </span>
             </p>
           </Reveal>
@@ -113,5 +119,5 @@ export const Project = (project: ProjectMDX) => {
       </motion.div>
       <ProjectModal project={project} isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
-  )
-}
+  );
+};
